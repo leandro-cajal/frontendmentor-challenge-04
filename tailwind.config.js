56ab530406257ed9d47d2df2ca3e05e5d79616f2
leandro cajal <leo_origin@hotmail.com>
2023-11-13 05:39:55 -0300

    end of the grid with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "main": ['Barlow Semi Condensed', 'sans-serif']
+      },
+      colors:{
+        "moderate-violet": "hsl(263, 55%, 52%)",
+        "very-dark-grayish-blue": "hsl(217, 19%, 35%)",
+        "very-dark-blackish-blue": "hsl(219, 29%, 14%)",
+        "white": "hsl(0, 0%, 100%)",
+        "light-gray": "hsl(0, 0%, 81%)",
+        "light-grayish-blue": "hsl(210, 46%, 95%)",
+      },
+    },
+  },
+  plugins: [],
+}
+
